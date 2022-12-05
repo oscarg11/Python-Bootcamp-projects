e893319656a8f8a7f6b33ecd37f4d1b6e90f810e
oscarg11 <oscar.g2511@gmail.com>
2022-12-05 18:46:17 -0500

    2nd attempt at these exercises

--- fundamentals/oop/bankAccount.py
+++ fundamentals/oop/bankAccount.py
@@ -1,84 +1,48 @@
 class BankAccount:
-
+    #store BankAccount instances(accounts) in list
     all_accounts = []
 
-    def __init__(self,int_rate,balance):
+    def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.all_accounts.append(self)
-    #This method increases the account balance by the given amount
-    def deposit(self, amount):
+    #deposit function
+    def deposit(self,amount):
         self.balance += amount
-        print(self.balance)
         return self
-    #prints all instances of a Bank accounts info
-    @classmethod
-    def get_bank_account_info(cls):
-        for account in cls.all_accounts:
-            print(account.display_account_info())
-        return cls
-        
-    #decreases the account balance by the given amount 
-    #if there are sufficient funds; if there is not enough money, 
-    #print a message "Insufficient funds: Charging a $5 fee" and deduct $5
-    def withdraw(self, amount):
-        if (self.balance - amount) >= 0:
-            self.balance -= amount
-            print(f"-{amount}")
-        else:
-            print("Insuficient funds: Charging a $5 fee")
-            self.balance -= 5
-        return self   
-    #This methods displays the account balance
+    #withdraw
+    def withdraw(self,amount):
+        self.balance -= amount
+        return self
+    #display info
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print("Your Balance is:",self.balance,"Your interest rate is:",self.int_rate, sep='\n')
         return self
-    
-    #increases the account balance by the current balance * the interest rate 
-    # (as long as the balance is positive)
+    #interest rate calculation
     def yield_interest(self):
-        if self.balance > 0:
-            self.balance = self.balance + (self.balance * self.int_rate)
-            return self
-
-#USER class: this class uses an Instance of the BankAccount class
-#by the name of self.account that uses methods that mirror the 
-#methods used in BankAccount. These methods call on those classes.
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
-    
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
+        self.balance += (self.balance * self.int_rate)
         return self
     
-    def make_withdraw(self, amount):
-        self.account.withdraw(amount)
-        return self
-    
-    def display_user_balance(self):
-        self.account.display_account_info()
-        return self
-    
-    
-#bankAccount instances
-account1 = BankAccount(0.03, 1000)
-account2 = BankAccount(0.05, 1000)
+    #NINJA BONUS: Use a classmethod to print all instances of a Bank Account's info
+    @classmethod
+    def get_all_accounts(cls):
+        for account in cls.all_accounts:
+            print(f"Balance:${account.balance}, Interest Rate:{account.int_rate}")
 
-account1.deposit(100).deposit(45).deposit(100).withdraw(500).yield_interest().display_account_info()
-print("----------------------")
-account2.deposit(600).deposit(600).withdraw(100).withdraw(80).withdraw(400).withdraw(200).yield_interest().display_account_info()
 
-#call class method
-print("----------------------")
-BankAccount.get_bank_account_info()
 
-#user class instance
-user1 = User(
-    name = "oscar g",
-    email = "oscar.g2511@gmail.com"
-)
-print("User class methods:")
-user1.make_deposit(100).make_deposit(200).make_withdraw(50).display_user_balance()
+#BankAccount Instances
+account1 = BankAccount(0.01, 0)
+account2 = BankAccount(0.01,0)
+
+#Make 3 deposits and 1 withdrawl, then yield interest and display the accounts info in one line of code
+print("ACCOUNT 1:")
+account1.deposit(100).deposit(100).deposit(100).withdraw(150).yield_interest().display_account_info()
+
+#Make 2 deposits and 4 withdrawls,then yield interest and display the accounts info in one line of code
+print("ACCOUNT 2:")
+account2.deposit(1000).deposit(1000).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
+
+#prints each instance of BankAccounts stored info
+print("ALL ACCOUNTS IN BankAccount:")
+BankAccount.get_all_accounts()
--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -1,71 +1,79 @@
 class User:
-    def __init__(self, first_name, last_name, email, age):
-        self.fname = first_name
-        self.lname = last_name
+    def __init__(self,first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
 
-#This method prints all of the users' details on separate lines
+#Have this method print all of the users details on seperate lines
     def display_info(self):
-        print(f'''            {self.fname}
-            {self.lname}
-            {self.email}
-            {self.age}
-            {self.is_rewards_member}
-            {self.gold_card_points}''')
-        return self  
-
-#This method changes the Users' member status to True 
-#and checks if user is already a member
-# and sets their gold points to 200.
-    def enroll(self):
-        self.is_rewards_member = True
-        self.gold_card_points = 200
-
-
-        if self.is_rewards_member == True:
-            print(f"{self.fname} is already a member")
-            return self
-        else: return False
-
-#This method decreases the Users' points by amount specified 
-#and prevents user from over spending points
-    def spend_points(self, amount):
-        if self.gold_card_points >= amount:
-            self.gold_card_points = self.gold_card_points - amount
-        else:
-            print("You do not have enough points")
+        print(self.first_name,self.last_name,self.email,self.age,self.is_rewards_member,self.gold_card_points, sep='\n')
         return self
 
-    
-#three instances of User:
-user1 = User("Oscar", "Garcia", "Oscar.g2511@gmail.com", 23)
-user2 = User("Cristiano", "Ronaldo", "Cr7@gmail.com", 36)
-user3 = User("Lionel", "Messi", "lmessi@psg.com",35)
+#Have this method change the user's member status to True and set their gold card points to 200
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print(f"User {self.first_name} is already a member")
+        else:
+            print(f"Welcome {self.first_name}! 200 points have been added to your account")
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+            return self
+            
+        # self.is_rewards_member = True
 
-#invoke methods on instances of User by chaining them
-user1.enroll().spend_points(50).display_info()
-print(" ")
-user2.enroll().spend_points(100).display_info()
-print(" ")
-user3.enroll().spend_points(150).display_info()
-print(" ")
+#Have this mehtod decrease the user's points by the amount specified
+    def spend_points(self,amount):
+        if amount > self.gold_card_points:
+            print("You do not have enough points")
+        else:
+            self.gold_card_points -= amount
+            return self
 
+#Instances of User class
+user1 = User("Oscar","Garcia","oscar@email.com",24)
+user2 = User("John","Doe","JohnD@gmail.com",32)
+user3 = User("Jimi","Hendrix","JimiHexperiece@gmail.com",27)
 
-# Attributes:
-# On instantiation of a user, the following attributes should be passed in as arguments:
+# print("DISPLAY USER1:")
+# user1.display_info()
 
-# first_name
-# last_name
-# email
-# age
-# Also include default attributes:
+# print("USER 1 ENROLLED:")
+# user1.enroll()
 
-# is_rewards_member - default value of False
-# gold_card_points = 0
-# Methods:
-# display_info(self) - Have this method print all of the users' details on separate lines.
-# enroll(self) - Have this method change the user's member status to True and set their gold card points to 200.
-# spend_points(self, amount) - have this method decrease the user's points by the amount specified.
+# print("DISPLAY USER1:")
+# user1.display_info()
+
+# #have user1 spend 50 points
+# print("HAVE USER1 SPEND 50 POINTS:")
+# user1.spend_points(50)
+
+# print("DISPLAY USER1:")
+# user1.display_info()
+
+# #enroll user2
+# print("USER 2 ENROLLED:")
+# user2.enroll()
+
+# #Have user2 spend 80 points
+# print("HAVE USER2 SPEND 80 POINTS:")
+# user2.spend_points(80)
+
+# #call the display method on all users
+# print("DISPLAY USER2:")
+# user2.display_info()
+# print("DISPLAY USER1:")
+# user1.display_info()
+
+# #try to re-enroll user1
+# print("TRY RE-ENROLLING USER1:")
+# user1.enroll()
+
+# #test spending limit on user3
+# print("TEST SPENDING LIMIT ON USER3:")
+# user3.spend_points(40)
+
+#chain methods together to save space
+user1.display_info().enroll().spend_points(50).display_info()
