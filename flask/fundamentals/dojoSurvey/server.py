c8dbf84b1e29c069c9dfb084b115fe1d46284f75
oscarg11 <oscar.g2511@gmail.com>
2022-12-23 11:10:37 -0500

    Second attempt at Dojo Survey

--- flask/fundamentals/dojoSurvey/server.py
+++ flask/fundamentals/dojoSurvey/server.py
@@ -6,27 +6,30 @@ app.secret_key = 'secret password'
 @app.route('/')
 def index():
     return render_template("index.html")
-#Saves user's input in session and redirects it so that it can be rendered on a page.
-@app.route('/process', methods=['POST'])
-def process():
-    print(request.form)
-    session['yourname'] = request.form['name']
-    session['dojolocation'] = request.form['location']
-    session['language'] = request.form['favlanguage']
-    #Only the selected radio button will be displayed
-    session['time-option']=request.form['time-options']
-    #checkboxes can be retrieved by calling the getlist method
-    session['checkedbox'] = request.form.getlist('checkbox')
-    session['comment'] = request.form['comment']
-    return redirect('/results')
-#recieves the user's input to be rendered on a result page.
-@app.route('/results')
-def results():
-    print(request.form)
-    return render_template('results.html',name_on_template=session['yourname'],
-    location_on_template=session['dojolocation'], language_on_template=session['language'],
-    checkbox_on_template=session['checkedbox'], comment_on_template=session['comment'])
 
+#process form input
+@app.route('/process', methods=['POST'])
+def process_form():
+    print("submitted info")
+    print(request.form)
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['favlanguage'] = request.form['favlanguage']
+    # Only the selected radio button will be displayed
+    session['time_options'] = request.form['time_options']
+    # check boxes can be accessed by calling the getList method
+    session['checkbox'] = request.form.getlist('checkbox')
+    session['comment'] = request.form['comment']
+
+    return redirect('/results')
+
+#redirect to display results page
+@app.route('/results')
+def display_results():
+    print("DISPLAYING USER FORM INFO")
+    print(request.form)
+    return render_template("results.html", name_on_template=session['name'], location_on_template=session['location'], favlanguage_on_template=session['favlanguage'],
+    radio_on_template=session['time_options'],checkbox_on_template=session['checkbox'],comment_on_template=session['comment'])
 
 if __name__ == "__main__":
     app.run(debug=True)
