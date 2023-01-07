bf1351e18e16eb1f2f95549527c861d2b2728adc
oscarg11 <oscar.g2511@gmail.com>
2023-01-07 00:19:18 -0500

    second attempt at user_cr assignment, which is the first project I've done by connecting a database and using sql queries. This time around I added better bootstrap features

--- flask_mysql/db_connection/users_cr/server.py
+++ flask_mysql/db_connection/users_cr/server.py
@@ -1,5 +1,40 @@
-from flask_app.controllers import users
-from flask_app import app
+from flask import Flask, render_template, request, redirect # Import Flask to allow us to create our app
+from users import User #import User class
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#root route
+@app.route('/')
+def index():
+    return redirect('/users')
+
+#call the get_all() method from users.py to display all users in db
+@app.route('/users')
+def users():
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users = users)
+
+
+@app.route('/users/new')
+def render_form_page():
+    return render_template("create.html")
+
+#process form data
+@app.route('/create_user', methods=["POST"])
+def create_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    User.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
---
+++ flask_mysql/db_connection/users_cr/users.py
@@ -0,0 +1,33 @@
+from mysqlconnection import connectToMySQL
+
+#model the class after the user table created in my database
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+#use class methods to query our data base
+
+# this methods selects all users in our db
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        #call the connectToMySQL function with the schema you are targeting 
+        results = connectToMySQL('users_schema').query_db(query)
+        # create an empty list to append our instances of users
+        users = []
+        # iterate over the db results and  create instances of friends with cls
+        for user in results:
+            users.append( cls(user) )
+        return users
+
+    #this method saves a user to our db
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users ( first_name, last_name, email) VALUES ( %(fname)s , %(lname)s , %(email)s);"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users_schema').query_db( query, data)
